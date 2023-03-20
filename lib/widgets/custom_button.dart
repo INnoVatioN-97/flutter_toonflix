78d6ff44e5d1af09604d20fd129574672f9e0a9d
YOUNGIL KO <koyoungil97@gmail.com>
2023-03-20 16:25:27 +0900

    공통컴포넌트 작업(버튼)

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final Color buttonColor;
+  final String buttonText;
+  final Color buttonTextColor;
+
+  const CustomButton(
+      {super.key,
+      required this.buttonColor,
+      required this.buttonText,
+      required this.buttonTextColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(45),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 22, color: buttonTextColor),
+        ),
+      ),
+    );
+  }
+}
